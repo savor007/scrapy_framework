043f71793827d43676a9b4c2a84592e4111efce6
Sun Wei <sunwei@SundeMacBook-Pro.local>
2019-09-03 11:19:32 +0800

    changes on midwares

---
+++ scrapy_framework/midwares/download_midware.py
@@ -0,0 +1,7 @@
+class DownloadMidware(object):
+    def process_request(self, request):
+        pass
+
+
+    def process_response(self, response):
+        pass
---
+++ scrapy_framework/midwares/scrapy_midware.py
@@ -0,0 +1,6 @@
+class ScrapyMidware(object):
+    def process_request(self, request):
+        pass
+
+    def process_response(self, response):
+        pass
